d9c6b7b350d087db79873383d8151cc2bce93838
bouchinaseifeddine <bouchinaseifeddine@gmail.com>
2024-05-25 15:00:56 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/views/home_view.dart';
+
+void main() {
+  runApp(const HomeView());
+}
---
+++ lib/views/home_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 100,
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
