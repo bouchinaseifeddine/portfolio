fa68ccaeefd2a19ba5e4022ee3bf69389b1b9674
bouchinaseifeddine <bouchinaseifeddine@gmail.com>
2024-05-30 22:52:59 +0100

    add adaptive layout

---
+++ lib/layout/tablet_layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TabletLayout extends StatelessWidget {
+  const TabletLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Tablet Layout'),
+    );
+  }
+}
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/layout/desktop_layout.dart';
+import 'package:portfolio/layout/mobile_layout.dart';
+import 'package:portfolio/layout/tablet_layout.dart';
+import 'package:portfolio/widgets/adaptive_layout.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 245, 243, 248),
-      body: Center(
-        child: Text('sayf'),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 245, 243, 248),
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const MobileLayout(),
+        tabletLayout: (context) => const TabletLayout(),
+        desktopLayout: (context) => const DesktopLayout(),
       ),
     );
   }
