783ada106757d6497b0a7f06c53badffc0cb4f0f
bouchinaseifeddine <bouchinaseifeddine@gmail.com>
2024-06-01 15:00:24 +0100

    add service model + services list

---
+++ lib/data/services.dart
@@ -0,0 +1,20 @@
+import 'package:portfolio/models/service.dart';
+
+final List<Service> services = [
+  Service(
+      title: 'Mobile Apps',
+      text: 'Professional development of applications for iOS and Android',
+      iconPath: 'assets/images/mobile.svg'),
+  Service(
+      title: 'Web Developement',
+      text: 'Building responsive and user-friendly web applications',
+      iconPath: 'assets/images/web.svg'),
+  Service(
+      title: 'Problem Solving',
+      text: 'Expertise in solving complex problems and challenges',
+      iconPath: 'assets/images/idea.svg'),
+  Service(
+      title: 'Gaming',
+      text: 'I like to spend my free time playing with friends!',
+      iconPath: 'assets/images/gaming.svg'),
+];
---
+++ lib/models/service.dart
@@ -0,0 +1,7 @@
+class Service {
+  final String title;
+  final String text;
+  final String iconPath;
+
+  Service({required this.title, required this.text, required this.iconPath});
+}
