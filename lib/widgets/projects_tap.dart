8fe7b666ecc1840d587133480300277a2edab26b
bouchinaseifeddine <bouchinaseifeddine@gmail.com>
2024-06-05 18:36:18 +0100

    add projects to desktop layout

--- lib/widgets/projects_tap.dart
+++ lib/widgets/projects_tap.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/data/projects.dart';
+
+import 'package:portfolio/utils/size_config.dart';
+import 'package:portfolio/widgets/project_item.dart';
 
 class ProjectsTap extends StatelessWidget {
   const ProjectsTap({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 3,
-      shrinkWrap: true,
-      childAspectRatio: 3,
-      mainAxisSpacing: 16,
-      crossAxisSpacing: 16,
-      physics: const NeverScrollableScrollPhysics(),
-      children: const [],
+    return Column(
+      children: [
+        SizedBox(height: SizeConfig.defaultSize),
+        GridView.count(
+          crossAxisCount: 3,
+          shrinkWrap: true,
+          childAspectRatio: 1,
+          mainAxisSpacing: 16,
+          crossAxisSpacing: 16,
+          physics: const NeverScrollableScrollPhysics(),
+          children: [
+            for (final project in projects) ProjectItem(project: project),
+          ],
+        ),
+      ],
     );
   }
 }
