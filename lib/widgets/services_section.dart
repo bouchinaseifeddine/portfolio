6c38f5faa3559c27ff84c9262a664437586397c9
bouchinaseifeddine <bouchinaseifeddine@gmail.com>
2024-06-06 19:54:28 +0100

    responsive about tap + fix the GrivView Height issue

--- lib/widgets/services_section.dart
+++ lib/widgets/services_section.dart
@@ -1,3 +1,4 @@
+import 'package:auto_height_grid_view/auto_height_grid_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:portfolio/data/services.dart';
@@ -10,6 +11,7 @@ class ServicesSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -18,54 +20,81 @@ class ServicesSection extends StatelessWidget {
           style: AppStyles.styleBold24(context),
         ),
         SizedBox(height: SizeConfig.defaultSize),
-        GridView.count(
-          crossAxisCount: 2,
+        AutoHeightGridView(
+          itemCount: services.length,
+          crossAxisCount: width < 800 ? 1 : 2,
           shrinkWrap: true,
-          childAspectRatio: 3,
           mainAxisSpacing: 16,
           crossAxisSpacing: 16,
           physics: const NeverScrollableScrollPhysics(),
-          children: [
-            for (final service in services)
-              GradientContainer(
-                padding: 30,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: SvgPicture.asset(
-                        service.iconPath,
-                        width: 40,
-                        height: 40,
-                      ),
-                    ),
-                    SizedBox(width: SizeConfig.defaultSize),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            service.title,
-                            style: AppStyles.styleSemiBold32(context)
-                                .copyWith(fontSize: 18),
-                          ),
-                          SizedBox(height: SizeConfig.defaultSize! / 2),
-                          Expanded(
-                            child: Text(
-                              service.text,
-                              style: AppStyles.styleLight15(context),
-                              maxLines: 3,
-                              overflow: TextOverflow.clip,
+          builder: (ctx, index) {
+            return IntrinsicHeight(
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(minHeight: 150),
+                child: GradientContainer(
+                  padding: 30,
+                  child: width < 800
+                      ? Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            FittedBox(
+                              fit: BoxFit.scaleDown,
+                              child: SvgPicture.asset(
+                                services[index].iconPath,
+                                width: 40,
+                                height: 40,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                            SizedBox(height: SizeConfig.defaultSize),
+                            Text(
+                              services[index].title,
+                              style: AppStyles.styleSemiBold32(context)
+                                  .copyWith(fontSize: 18),
+                            ),
+                            SizedBox(height: SizeConfig.defaultSize),
+                            Text(
+                              services[index].text,
+                              style: AppStyles.styleLight15(context),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        )
+                      : Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            FittedBox(
+                              fit: BoxFit.scaleDown,
+                              child: SvgPicture.asset(
+                                services[index].iconPath,
+                                width: 40,
+                                height: 40,
+                              ),
+                            ),
+                            SizedBox(width: SizeConfig.defaultSize),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    services[index].title,
+                                    style: AppStyles.styleSemiBold32(context)
+                                        .copyWith(fontSize: 18),
+                                  ),
+                                  SizedBox(height: SizeConfig.defaultSize! / 2),
+                                  Text(
+                                    services[index].text,
+                                    style: AppStyles.styleLight15(context),
+                                    maxLines: 2,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                 ),
               ),
-          ],
+            );
+          },
         )
       ],
     );
