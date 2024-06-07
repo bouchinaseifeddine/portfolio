752de94c685e5c52680c7c7c0ecf710220294934
bouchinaseifeddine <bouchinaseifeddine@gmail.com>
2024-06-07 12:12:24 +0100

    smtp config

--- lib/utils/functions/send_email.dart
+++ lib/utils/functions/send_email.dart
@@ -8,16 +8,15 @@ Future sendEmail({
   required String message,
 }) async {
   final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
-  await http.post(
+  final response = await http.post(
     url,
     headers: {
       'Content-Type': 'application/json',
     },
     body: json.encode({
-      // enter your account details here
-      'service_id': 'SERVICE_ID_HERE',
-      'template_id': 'TEMPLATE_ID_HERE',
-      'user_id': 'USER_ID_HERE',
+      'service_id': 'service_xb9a89m',
+      'template_id': 'template_9t2utc5',
+      'user_id': 'aq7EOGBpSsY1CyTxk',
       'template_params': {
         'user_name': name,
         'user_email': email,
